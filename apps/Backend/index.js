85f258dd4c71a4292980f2c3d55a7399d35ad85f
Vishnu9804 <burkhawalavishnu@gmail.com>
2024-12-20 18:00:04 +0530

    Initial Commit

---
+++ apps/Backend/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const port = 3000;
+const port1 = 3001;
+const app = express();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(authRoutes);
+
+app.listen(port, () => {
+  console.log("Server is running on post " + port);
+});
