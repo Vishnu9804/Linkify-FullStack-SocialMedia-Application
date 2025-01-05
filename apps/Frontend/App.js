1c176f65902b9162446a5d068c1e1ce09817a202
Vishnu9804 <burkhawalavishnu@gmail.com>
2025-01-05 22:12:19 +0530

    Navigation Stack for Signup in App.js

--- apps/Frontend/App.js
+++ apps/Frontend/App.js
@@ -1,12 +1,68 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{ headerShown: false, animation: "slide_from_right" }}
+        initialRouteName="SplashScreen"
+      >
+        <Stack.Screen name="SplashScreen" component={SplashScreen} />
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="MainPage" component={Mainpage} />
+        <Stack.Screen name="Signup_EnterEmail" component={Signup_EnterEmail} />
+        <Stack.Screen
+          name="Signup_EnterVerificationCode"
+          component={Signup_EnterVerificationCode}
+        />
+        <Stack.Screen
+          name="Signup_ChooseUsername"
+          component={Signup_ChooseUsername}
+        />
+        <Stack.Screen
+          name="Signup_ChoosePassword"
+          component={Signup_ChoosePassword}
+        />
+        <Stack.Screen
+          name="Signup_AccountCreated"
+          component={Signup_AccountCreated}
+        />
+
+        <Stack.Screen
+          name="ForgotPassword_AccountRecovered"
+          component={ForgotPassword_AccountRecovered}
+        />
+        <Stack.Screen
+          name="ForgotPassword_ChoosePassword"
+          component={ForgotPassword_ChoosePassword}
+        />
+        <Stack.Screen
+          name="ForgotPassword_EnterEmail"
+          component={ForgotPassword_EnterEmail}
+        />
+        <Stack.Screen
+          name="ForgotPassword_EnterVerificationCode"
+          component={ForgotPassword_EnterVerificationCode}
+        />
+        <Stack.Screen
+          name="All_Chats"
+          component={All_Chats}
+          options={{
+            animation: "slide_from_right",
+          }}
+        />
+        <Stack.Screen name="Mainpage" component={Mainpage} />
+        <Stack.Screen
+          name="SearchUserPage"
+          component={SearchUserPage}
+          options={{
+            animation: "fade_from_bottom",
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
